277425d876b6c6d34db5ed1111d5d981edab66b2
RodrigoG <eduis.carranza123@gmail.com>
2024-06-24 00:35:09 -0500

    build del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [react(), sitemap()],
   site: 'https://rodriguzmanc.github.io/Portfolio',
   base: '/Portfolio/',
   output: 'static'
